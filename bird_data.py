32b9f58e33e9b06e01e74936170a358cfd984d9b
ryanmcritchieking <ryan.mcritchie4@gmail.com>
2025-08-04 12:26:34 +1200

    this gets the the doc open

--- bird_data.py
+++ bird_data.py
@@ -1,3 +1,11 @@
 import json
 import os
 import re
+
+data="data.json"
+
+if os.path.exists(data):
+    with open(data, 'r') as file:
+        onsitedata = json.load(file)
+else:
+    onsitedata = {}
