0643794ca2837dd4014e2a0b1ba2436040ea91d4
ryanmcritchieking <ryan.mcritchie4@gmail.com>
2025-08-04 12:29:25 +1200

    added a menu

--- bird_data.py
+++ bird_data.py
@@ -29,3 +29,36 @@ def remove_bird_data():
 
 def show_bird_data():
     print("nothing hear")
+
+
+
+def show_menu():
+    while True:
+        print('bird data saver')
+        print('add bird:1')
+        print('add more than onebird:2')
+        print('remove bird data:3')
+        print('show bird data:4')
+        option=input("pick from 1-4: ")
+        if option=='1':
+            add_bird_data()
+        elif option=='2':
+            add_lots_bird_data()
+        elif option=='3':
+            remove_bird_data()
+        elif option=='4':
+            show_bird_data()
+        elif option=='5':
+            print("nothing hear to update")
+        elif option=='6':
+            print("nothing hear to update")
+        else:
+            print("this is not a number to choose")
+            
+        
+
+
+
+
+
+show_menu()
