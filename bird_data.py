d769e1df2619f2768ee4d01bac33b09555eaa53f
ryanmcritchieking <ryan.mcritchie4@gmail.com>
2025-08-19 13:21:28 +1200

    test

--- bird_data.py
+++ bird_data.py
@@ -87,7 +87,7 @@ def exsport_to_excel():
             'Location': where_found,
             'When': when_seen
         })
-df = pd.DataFrame(bird_list)
+df = pd.DataFrame("bird_list")
 df.to_excel("bird_data.xlsx", index=False)
 print("Bird data sentto bird_data.xlsx")
 
